b6f7a88d361a1d665410c6ec2238d95d325875f2
Alana Alexander <laninsky@gmail.com>
2018-01-12 12:44:48 +1300

    Update monolithic.sh

--- monolithic.sh
+++ monolithic.sh
@@ -5,7 +5,7 @@ no_fastas=`wc -l fastalist.txt | awk '{ print$1 }'`
 # Separating out monolithic fastas by taxa the loci is characterized in (further grouped by basegenome used)
 for i in `seq 1 $no_fastas`; do current_file=`head -n $i fastalist.txt | tail -n 1`; mv $current_file temp; echo $current_file > current_name; Rscript split_fasta_by_taxa.R; mv temp $current_file; done
 
-# Description
+# This loop is going into each taxon's folder and doing a blast between the fastas recovered using different basegenomes
 basewd=`pwd`
 for i in `ls -d */`;
 do cd $i;
@@ -25,13 +25,7 @@ done;
 cd $basewd;
 done
 
+# Summarizing across the blast files for each taxa to determine presence/absence of each locus in each taxon 
+# predicated on which base genome was used.
 
-
-# Then go into each folder and blast across genomes. Save this info per taxon
-
-
-
-for i in taxa*fasta; do ... cross blast 
-
-# Then take these files and combine them across taxa
-
+Rscript blast_summary.R
