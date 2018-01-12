a4515a26cb2aad4517f606a76c9c4bb7e7492c34
Alana Alexander <laninsky@gmail.com>
2018-01-12 15:56:44 +1300

    Update blast_summary.R

--- blast_summary.R
+++ blast_summary.R
@@ -18,7 +18,27 @@ for (i in list.files(pattern="_blast.txt",recursive=TRUE)) {
           # we have a problem because the locus from the [j,1] base genome is matching to multiple others at the blast threshold we used
           } else { #10AB
             output_matrix[which(output_matrix[,which(output_matrix[1,]==temp[j,1])]==temp[j,3]),which(output_matrix[1,]=="problem_locus")] <- "Y"
-            break
+            temp_row <- t(matrix(NA,nrow=dim(output_matrix)[2]))
+            temp_row[1,which(output_matrix[1,]=="problem_locus")] <- "Y"
+            temp_row[1,which(output_matrix[1,]==temp[j,1])] <- temp[j,3]
+            temp_row[1,which(output_matrix[1,]==temp[j,2])] <- temp[j,4]
+            # What to do if both base genomes have equally long loci
+            if(as.numeric(temp[j,5])==as.numeric(temp[j,6])) { #2A
+              temp_row[1,which(output_matrix[1,]=="max_length")] <- as.numeric(temp[j,5])
+              temp_row[1,which(output_matrix[1,]=="which_base_gives_max")] <- paste("TIE_",gsub("/ucelocus.txt","",temp[j,1],fixed=TRUE),"_",gsub("/ucelocus.txt","",temp[j,2],fixed=TRUE),sep="")
+            # What to do if genomes DON'T have the same length loci
+            } else { #2AB
+              # What to do if temp[j,5] is long than temp[j,6]
+              if(as.numeric(temp[j,5])>as.numeric(temp[j,6])) { #1A
+                temp_row[1,which(output_matrix[1,]=="max_length")] <- as.numeric(temp[j,5])
+                temp_row[1,which(output_matrix[1,]=="which_base_gives_max")] <- gsub("/ucelocus.txt","",temp[j,1],fixed=TRUE)
+              # What to do if temp[j,6] is long than temp[j,5]
+              } else {  #1AB
+                temp_row[1,which(output_matrix[1,]=="max_length")] <- as.numeric(temp[j,6])
+                temp_row[1,which(output_matrix[1,]=="which_base_gives_max")] <- gsub("/ucelocus.txt","",temp[j,2],fixed=TRUE)
+              } #1B
+           } #2B
+           output_matrix <- rbind(output_matrix,temp_row)                
           }  #10B
         } #5B
      # if temp[j,3] NOT already in table   
