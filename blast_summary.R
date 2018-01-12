693f15a2e773b7b2a99846777b20fb5c64070b38
Alana Alexander <laninsky@gmail.com>
2018-01-12 16:15:31 +1300

    Update blast_summary.R

--- blast_summary.R
+++ blast_summary.R
@@ -9,7 +9,30 @@ for (i in list.files(pattern="_blast.txt",recursive=TRUE)) {
      if (temp[j,3] %in% output_matrix[1:(dim(output_matrix)[1]),which(output_matrix[1,]==temp[j,1])]) { #4A
         # TO DO what to do if both [j,3] and [j,4] are already there (shouldn't ever happen)
         if (temp[j,4] %in% output_matrix[1:(dim(output_matrix)[1]),which(output_matrix[1,]==temp[j,2])]) { #5A
-          break
+          output_matrix[which(output_matrix[,which(output_matrix[1,]==temp[j,1])]==temp[j,3]),which(output_matrix[1,]=="problem_locus")]
+          output_matrix[which(output_matrix[,which(output_matrix[1,]==temp[j,2])]==temp[j,4]),which(output_matrix[1,]=="problem_locus")] <- "Y"
+          temp_row <- t(matrix(NA,nrow=dim(output_matrix)[2]))
+          temp_row[1,which(output_matrix[1,]=="problem_locus")] <- "Y"
+          temp_row[1,which(output_matrix[1,]==temp[j,1])] <- temp[j,3]
+          temp_row[1,which(output_matrix[1,]==temp[j,2])] <- temp[j,4]
+          # What to do if both base genomes have equally long loci
+          if(as.numeric(temp[j,5])==as.numeric(temp[j,6])) { #2A
+            temp_row[1,which(output_matrix[1,]=="max_length")] <- as.numeric(temp[j,5])
+            temp_row[1,which(output_matrix[1,]=="which_base_gives_max")] <- paste("TIE_",gsub("/ucelocus.txt","",temp[j,1],fixed=TRUE),"_",gsub("/ucelocus.txt","",temp[j,2],fixed=TRUE),sep="")
+            # What to do if genomes DON'T have the same length loci
+            } else { #2AB
+            # What to do if temp[j,5] is long than temp[j,6]
+            if(as.numeric(temp[j,5])>as.numeric(temp[j,6])) { #1A
+              temp_row[1,which(output_matrix[1,]=="max_length")] <- as.numeric(temp[j,5])
+              temp_row[1,which(output_matrix[1,]=="which_base_gives_max")] <- gsub("/ucelocus.txt","",temp[j,1],fixed=TRUE)
+            # What to do if temp[j,6] is long than temp[j,5]
+            } else {  #1AB
+              temp_row[1,which(output_matrix[1,]=="max_length")] <- as.numeric(temp[j,6])
+              temp_row[1,which(output_matrix[1,]=="which_base_gives_max")] <- gsub("/ucelocus.txt","",temp[j,2],fixed=TRUE)
+             } #1B
+           } #2B
+           output_matrix <- rbind(output_matrix,temp_row)  
+          } #10B
         # TO DO  what to do if temp[j,3] is already in there, but temp[j,4] is not  
         } else { #5AB
           # if there is no value in the output_matrix for the [j,2] base genome
