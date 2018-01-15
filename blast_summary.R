d94c9cad3337d33ca8a48f0fb4a526016ffe3463
Alana Alexander <laninsky@gmail.com>
2018-01-15 13:11:01 +1300

    Update blast_summary.R

--- blast_summary.R
+++ blast_summary.R
@@ -36,12 +36,25 @@ for (i in list.files(pattern="_blast.txt",recursive=TRUE)) {
         } else { #5AB
           # if there is no value in the output_matrix for the [j,2] base genome
           if(all(is.na(output_matrix[(which(output_matrix[,which(output_matrix[1,]==temp[j,1])]==(temp[j,3]))),which(output_matrix[1,]==temp[j,2])]))) { #10A
-            break
-            output_matrix[(which(output_matrix[,which(output_matrix[1,]==temp[j,1])]==(temp[j,3]))),which(output_matrix[1,]==temp[j,2])] <- temp[j,4]
-            # UP TO HERE - TRYING TO WORK OUT HOW TO PASTE IN TIES WHEN THERE ARE MULTIPLE VALUES - MIGHT HAVE TO DO A WEE FOR LOOP
-#            if(as.numeric(temp[j,6])==as.numeric(output_matrix[(which(output_matrix[,which(output_matrix[1,]==temp[j,1])]==(temp[j,3]))),which(output_matrix[1,]=="max_length")])) {
- #             output_matrix[(which(output_matrix[,which(output_matrix[1,]==temp[j,1])]==(temp[j,3]))),which(output_matrix[1,]=="which_base_gives_max")] <- paste(output_matrix[(which(output_matrix[,which(output_matrix[1,]==temp[j,1])]==(temp[j,3]))),which(output_matrix[1,]=="which_base_gives_max")],"_",gsub("/ucelocus.txt","",temp[j,2],fixed=TRUE),sep="")
-          # we have a problem because the locus from the [j,1] base genome is matching to multiple others at the blast threshold we used
+            # for each of the rows in output_matrix that match this
+            for (k in which(output_matrix[,which(output_matrix[1,]==temp[j,1])]==(temp[j,3]))) { #20A
+              output_matrix[k,which(output_matrix[1,]==temp[j,2])] <- temp[j,4]
+              # If the new base genome gives the same locus length
+              if (as.numeric(output_matrix[k,which(output_matrix[1,]=="max_length")])==temp[j,6]) { #21A
+                # If it already has tie in the title
+                if (grepl("TIE_",output_matrix[k,which(output_matrix[1,]=="which_base_gives_max")])) { #22A
+                  output_matrix[k,which(output_matrix[1,]=="which_base_gives_max")] <- paste(output_matrix[k,which(output_matrix[1,]=="which_base_gives_max")],"_",gsub("/ucelocus.txt","",temp[j,2],fixed=TRUE),sep="")
+                } else { #22AB
+                  output_matrix[k,which(output_matrix[1,]=="which_base_gives_max")] <- paste("TIE_",output_matrix[k,which(output_matrix[1,]=="which_base_gives_max")],"_",gsub("/ucelocus.txt","",temp[j,2],fixed=TRUE),sep="")
+                } #22B
+              } else { #21AB
+                if (as.numeric(output_matrix[k,which(output_matrix[1,]=="max_length")])<as.numeric(temp[j,6])) { #23A
+                  output_matrix[k,which(output_matrix[1,]=="which_base_gives_max")] <- gsub("/ucelocus.txt","",temp[j,2],fixed=TRUE)
+                  output_matrix[k,which(output_matrix[1,]=="max_length")] <- as.numeric(temp[j,6])
+                } #23B
+              } #21B
+           } #20B  
+          # we have a problem because the locus from the [j,1] base genome is matching to multiple others at the blast threshold we used
           } else { #10AB
             output_matrix[which(output_matrix[,which(output_matrix[1,]==temp[j,1])]==temp[j,3]),which(output_matrix[1,]=="problem_locus")] <- "Y"
             temp_row <- t(matrix(NA,nrow=dim(output_matrix)[2]))
