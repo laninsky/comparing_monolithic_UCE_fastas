14a96eccc516cabf794d588173819b706b6814c8
Alana Alexander <laninsky@gmail.com>
2018-01-18 17:14:18 +1300

    Update combining_across_taxa.R

--- combining_across_taxa.R
+++ combining_across_taxa.R
@@ -1,4 +1,10 @@
 file_list <- list.files(pattern=".summarized",recursive=TRUE)
+taxa_list <- unlist(strsplit(file_list,"/",fixed=TRUE))
+taxa_list <- taxa_list[seq((length(taxa_list)/length(file_list)),length(taxa_list),(length(taxa_list)/length(file_list)))]
+taxa_list <- unlist(strsplit(taxa_list,"_blast.txt.summarized")) 
+
+length_list
+
 
 temp_output <- as.matrix(read.table(file_list[1]))
 problem_loci_coords <- which(temp_output[,(which(temp_output[1,]=="problem_locus"))]=="Y")
