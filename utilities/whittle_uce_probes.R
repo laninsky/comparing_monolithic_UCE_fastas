31dfe35fe0c80f94d7df78bcd27c91f2932d11eb
Alana Alexander <laninsky@gmail.com>
2018-09-26 11:42:24 +1200

    Update whittle_uce_probes.R

--- utilities/whittle_uce_probes.R
+++ utilities/whittle_uce_probes.R
@@ -1,7 +1,10 @@
 probe_fasta_file <- "/Users/alanaalexander/Dropbox/beetles/grey_whittled_probes/Adephaga_11Kv1.fasta"
+monolithic_output_name <- "/Users/alanaalexander/Dropbox/beetles/grey_whittled_probes/output_matrix_99.txt"
+basename <- "Pterostichus.1"
 
-whittle_uce_probes <- function(target_loci,probe_fasta_file) {
+whittle_uce_probes <- function(monolithic_output_name,probe_fasta_file,basename) {
   
+  if (!require('dplyr')) install.packages('dplyr'); library('dplyr')
   
   # Reading in probe_fasta_file
   temp <- readLines(probe_fasta_file)
@@ -25,8 +28,40 @@ whittle_uce_probes <- function(target_loci,probe_fasta_file) {
   }
   outputmatrix[(dim(outputmatrix)[1]),] <- tempseq
   
+  # Reading in the monolithic output
+  temp <- read.table(monolithic_output_name,stringsAsFactors=FALSE,header=TRUE)
+  
+  # Filtering out loci that have between_taxa_problems
+  temp <- filter(temp,is.na(between_taxa_problem))
+  
+  # Filtering out loci that have problems within taxa
+  for (i in grep("_longest_base_genome",names(temp))) {
+    temp <- filter(temp,temp[,i]!="problem_within")
+  }  
+  
+  # Taking just the uce-locus names for the basename in our probe_fasta_file
+  temp <- select(temp,grep(basename,names(temp))[1])
+  
+  # Removing any is.na rows
+  temp <- filter(temp,!is.na(temp[,1]))
+  
+  # Getting the lines of the probe file that are found in our list of "keeper" loci
+  headerlines <- NULL
+  
+  for (i in 1:dim(temp)[1]) {
+    headerlines <- c(headerlines,which(grepl(temp[i,1],outputmatrix[,1])))
+    if ((i %% 100)==0) {
+     print(paste("We are ",round((i/dim(temp)[1])*100),"% through the file",sep=""))
+    }
+  }  
+  
+  headerlines <- unique(headerlines)
+  headerlines <- c(headerlines,(headerlines+1))
+  outputmatrix <- outputmatrix[headerlines,]
+  
+  write.table(outputmatrix,"whittled_UCE_probes.fasta",col.names=FALSE,row.names=FALSE,quotes=FALSE)
+}  
   
   
-  seq_or_no[x
   
   
