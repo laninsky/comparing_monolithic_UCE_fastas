26ac5188f7cf0d346fede7285710a1c087357728
Alana Alexander <laninsky@gmail.com>
2019-10-25 10:26:12 +1300

    Update monolithic.sh
    Adding in some more comments to explain why the last folder doesn't have a blast database

--- monolithic.sh
+++ monolithic.sh
@@ -11,10 +11,15 @@ for i in `cat fastalist.txt | sed 's/.insilico.incomplete.fasta//g'`;
 do cd $i;
 ls -d */ > base_genomes.txt;
 no_base_genomes=`wc -l base_genomes.txt | awk '{ print$1 }'`;
+# For every base_genome except the last one create a blast database
+# because it will be compared to the base_genomes that came
+# before, and we don't want duplicated comparisons
 for j in `seq 1 $(( no_base_genomes - 1 ))`;
 do blaster="`head -n $j base_genomes.txt | tail -n 1`"ucelocus.txt"";
 $BLASTPATH/makeblastdb -in $blaster -dbtype nucl;
 outfile_name="`echo $i | sed 's/\///g'`"_blast.txt"";
+# Take this blast database and compare it to every "subsequent"
+# base genome (i.e. do not do the reciprocal blasts that have already occured)
 for k in `seq $(( j + 1 )) $no_base_genomes`;
 do blastee="`head -n $k base_genomes.txt | tail -n 1`"ucelocus.txt"";
 $BLASTPATH/blastn -db $blaster -query $blastee -perc_identity $BLASTSIM -outfmt '10 sseqid qseqid slen qlen' > temp;
