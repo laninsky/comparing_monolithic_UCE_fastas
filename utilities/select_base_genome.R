cf8b10c34fc7298778fbf51e463d1e53a3481901
Alana Alexander <laninsky@gmail.com>
2022-07-05 08:52:31 +1200

    Update select_base_genome.R
    Changing read_table2 to read.table due to the mix of datatypes within the length categories. Also tidying up the longest recovered sequences bit.

--- utilities/select_base_genome.R
+++ utilities/select_base_genome.R
@@ -9,13 +9,13 @@ select_base_genome <- function(output_matrix_file) {
   
   # Loading required libraries
   if (!require('tidyverse')) install.packages('tidyverse'); library('tidyverse')
-
+  
   # Reading in output_matrix_file
-  temp <- read_table2(output_matrix_file,col_names = TRUE)
+  temp <- read.table(output_matrix_file,header = TRUE)
   
   # Getting taxa_names
   taxa_names <- gsub("_longest_base_genome","",names(temp)[grepl("_longest_base_genome",names(temp))])
-
+  
   # Filtering out loci that are problematic between/within base genomes
   temp <- filter(temp,is.na(between_taxa_problem))
   
@@ -25,6 +25,7 @@ select_base_genome <- function(output_matrix_file) {
   for (i in columns_to_check) {
     temp <- temp %>% filter(temp[,i]!="problem_within" & !is.na(temp[,i]))
   }  
+  
   
   # Base genome names
   base_genome_names <- names(temp)[grepl("ucelocus.txt$",names(temp))]
@@ -50,10 +51,10 @@ select_base_genome <- function(output_matrix_file) {
   
   # Which base genomes result in the longest recovered sequences across taxa
   print("The following proportion of loci had each base genome lead to the longest alignment (larger is better):")
-  base_genome_names  <- gsub("/ucelocus.txt","",base_genome_names)
+  base_genome_names  <- gsub(".ucelocus.txt","",base_genome_names)
   longest_base_genomes <- which(grepl("_longest_base_genome$",names(temp)))
   for (i in 1:length(base_genome_names)) {
-    av_count <- sum(str_count(as.matrix(temp[,longest_base_genomes]),fixed(base_genome_names[i])),na.rm=TRUE)/(dim(temp)[1]*length(taxa_names))
+    av_count <- sum(str_count(as.matrix(temp[,longest_base_genomes]),as.character(fixed(base_genome_names[i]))),na.rm=TRUE)/(dim(temp)[1]*length(taxa_names))
     print(paste(av_count," proportion of loci*taxa combinations where the following base genome gave longest alignment: ",base_genome_names[i],sep=""))
   }
   print("")
